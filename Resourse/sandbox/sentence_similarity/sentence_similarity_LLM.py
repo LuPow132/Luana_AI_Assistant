1494f7e67b3b9d9aa680f8e6f3d18aafefcd0eea
LuPow132 <68886157+LuPow132@users.noreply.github.com>
2023-09-22 15:51:08 +0700

    Update sentence_similarity_LLM.py

--- Resourse/sandbox/sentence_similarity/sentence_similarity_LLM.py
+++ Resourse/sandbox/sentence_similarity/sentence_similarity_LLM.py
@@ -1,29 +1,32 @@
 from sentence_transformers import SentenceTransformer, util
 import torch
+import json
 
-embedder = SentenceTransformer('all-MiniLM-L6-v2')
+model = SentenceTransformer('all-MiniLM-L6-v2')
 
 # Corpus with example sentences
-conversationDB = ['A man is eating food.',
-          'A man is eating a piece of bread.',
-          'The girl is carrying a baby.',
-          'A man is riding a horse.',
-          'A woman is playing violin.',
-          'Two men pushed carts through the woods.',
-          'A man is riding a white horse on an enclosed ground.',
-          'A monkey is playing drums.',
-          'A cheetah is running behind its prey.'
-          ]
-vectordb = embedder.encode(conversationDB, convert_to_tensor=True)
-
-# Query sentences:
-queries = ['A man is eating pasta.', 'Someone in a gorilla costume is playing a set of drums.', 'A cheetah chases prey on across a field.']
+conversationDB = []
 
 
-# Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
-top_k = min(5, len(vectordb))
-for query in queries:
-    query_embedding = embedder.encode(query, convert_to_tensor=True)
+def conversationLoad():
+    global conversationDB, vectordb
+    # Replace 'your_file.jsonl' with the actual filename
+    with open('source/conversation.jsonl', 'r') as file:
+        for line in file:
+            # Load each line as a JSON object and append it to the list
+            data = json.loads(line)
+            conversationDB.append(data['person'] + ":" + data['message'])
+    vectordb = model.encode(conversationDB, convert_to_tensor=True)
+
+conversationLoad()
+while True:
+    # Query sentences:
+    query = input("input keyword: ")
+
+
+    # Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
+    top_k = min(5, len(vectordb))
+    query_embedding = model.encode(query, convert_to_tensor=True)
 
     # We use cosine-similarity and torch.topk to find the highest 5 scores
     cos_scores = util.cos_sim(query_embedding, vectordb)[0]
@@ -31,15 +34,7 @@ for query in queries:
 
     print("\n\n======================\n\n")
     print("Query:", query)
-    print("\nTop 5 most similar sentences in corpus:")
+    print("\nTop 5 most similar sentences in corpus:\n\n")
 
     for score, idx in zip(top_results[0], top_results[1]):
         print(conversationDB[idx], "(Score: {:.4f})".format(score))
-
-    """
-    # Alternatively, we can also use util.semantic_search to perform cosine similarty + topk
-    hits = util.semantic_search(query_embedding, corpus_embeddings, top_k=5)
-    hits = hits[0]      #Get the hits for the first query
-    for hit in hits:
-        print(corpus[hit['corpus_id']], "(Score: {:.4f})".format(hit['score']))
-    """
